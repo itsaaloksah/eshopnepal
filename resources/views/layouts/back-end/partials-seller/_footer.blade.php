0e946766ffc98050b7aa01ff135c0c3368adb188
Aalok Sah <aalok.sah25@gmail.com>
2023-10-10 19:15:01 +0545

    corrected footer of seller - copyright

--- resources/views/layouts/back-end/partials-seller/_footer.blade.php
+++ resources/views/layouts/back-end/partials-seller/_footer.blade.php
@@ -2,7 +2,8 @@
 <div class="footer">
     <div class="row justify-content-between align-items-center">
         <div class="col mt-3">
-            <span>{{\App\CPU\translate('Copyright')}} &copy; {{url('/')}} {{date('Y')}}</span>
+             <!--<span>{{\App\CPU\translate('Copyright')}} &copy; {{url('/')}} {{date('Y')}}</span>-->
+             &copy; <span>{{\App\Model\BusinessSetting::where(['type'=>'company_copyright_text'])->first()->value}}</span>
         </div>
     </div>
 </div>
